14f7e97256154bbeb2f10782d06602b80bd31085
Aman Kumar <amankumar.spj410@gmail.com>
2022-07-16 22:41:55 +0530

    added login state

--- src/firebase/auth/index.js
+++ src/firebase/auth/index.js
@@ -1,15 +1,7 @@
 import {firebaseApp} from "../index"
-import {getAuth, onAuthStateChanged} from "firebase/auth"
-import SnackbarUtils from "../../components/SnackbarUtils"
+import {getAuth} from "firebase/auth"
 
 const auth = getAuth(firebaseApp)
-onAuthStateChanged(auth,(user) => {
-    if (user) {
-        SnackbarUtils.success("Logged In .")
-    } else {
-        SnackbarUtils.toast("Please Sign In .")
-    }
-})
 
 export default auth
 export * from "./"
